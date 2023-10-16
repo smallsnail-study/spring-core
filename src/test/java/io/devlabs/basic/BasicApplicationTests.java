00a50dd51c5d36a1666a16cf581ff8c78e1e7465
yunhuiGwak <kwakyh17@naver.com>
2023-10-16 14:26:49 +0900

    init: 스프링 핵심 원리 이해1 예제 프로젝트 초기화 - 실제 요구사항이 나중에 변경이 됐을 때 유연하게 대처가 가능한지 - 앞서 배운 다형성, OCP, DIP가 잘 지켜지고 있는지를 확인 - 문제가 있으면 다음 섹션인 스프링 핵심 원리 이해2에서 객체지향의 원리를 적용 해가며 문제들을 해결할 예정  - (뒤에서 관련 스프링 라이브러리들을 사용할 예정이라 순수 자바 프로젝트지만 스프링으로 생성)

---
+++ src/test/java/io/devlabs/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devlabs.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
